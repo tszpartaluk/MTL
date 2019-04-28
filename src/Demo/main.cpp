357363ac199e0f103f6f543c1ed921e47e1332a3
Tomasz Szpartaluk <tomasz.szpartaluk@gmail.com>
2019-04-28 15:22:39 +0200

    Add files

---
+++ src/Demo/main.cpp
@@ -0,0 +1,3 @@
+
+
+#include "MTL/list.h"
---
+++ src/MTL/MTL.cpp
@@ -0,0 +1,12 @@
+﻿// MTL.cpp : Defines the entry point for the application.
+//
+
+#include "MTL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
